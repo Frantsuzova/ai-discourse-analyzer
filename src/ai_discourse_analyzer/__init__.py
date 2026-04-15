476bef0617496b2d1c3604edc3bc5ad155d6019f
Anna Chizhik <53980712+Frantsuzova@users.noreply.github.com>
2026-04-16 01:40:36 +0300

    Add package name constants to init file

--- src/ai_discourse_analyzer/__init__.py
+++ src/ai_discourse_analyzer/__init__.py
@@ -3,3 +3,6 @@ from .pipeline import DiscourseAnalyzer, AnalysisResult
 
 __all__ = ["AnalysisConfig", "DiscourseAnalyzer", "AnalysisResult"]
 __version__ = "0.1.0"
+
+PACKAGE_SHORT_NAME = "AIDA"
+PACKAGE_FULL_NAME = "AI Discourse Analyzer"
